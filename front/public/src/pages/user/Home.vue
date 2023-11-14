5fc8b81f66bde42cfd75d5c1877ed30d09098255
Aleksandr-1987 <aleksander13@ukr.net>
2023-11-14 20:09:26 +0200

    new_new

--- front/public/src/pages/user/Home.vue
+++ front/public/src/pages/user/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <h1>Home page</h1>    
     <div class="category_title">Все отзывы:</div>  
-    <div>Some new</div>    
+    <div>Some new222</div>    
     <ul>                
         <li v-for="post in Review.reviews" :key="post.id" class="category_item">
             {{ post.name }} {{ post.surname }} {{ post.city }} {{ post.content }}                
