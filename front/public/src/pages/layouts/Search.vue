aa0f471523474f00779e52803a386bf0e80eaddd
Aleksandr-1987 <johndoe@example.com>
2024-01-05 21:29:16 +0200

    Pagination

---
+++ front/public/src/pages/layouts/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <select v-model="data.typeofwork" class="login_form_item auth_arrow">
+        <option v-for="option in typework" :value="option.id">
+            {{ option.title[locale] }}
+        </option>
+    </select>
+    <select v-model="data.city" class="login_form_item auth_arrow">
+        <option v-for="option in city" :value="option.id">
+            {{ option.title[locale] }}
+        </option>
+    </select>
+</template>
+
+<script>
+import { useI18n } from 'vue-i18n';
+export default {
+    name: "Search",
+    setup() {
+        const { t, locale } = useI18n({ useScope: 'global' });
+        return { t, locale };
+    },
+    props: {
+        data: Object,
+        typework: Object,
+        city: Object,
+    }
+}
+</script>
