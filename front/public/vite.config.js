e62abe0169bebb52742ae0239ecc059906c60a85
Aleksandr-1987 <johndoe@example.com>
2024-02-08 19:47:28 +0200

    SSS

--- front/public/vite.config.js
+++ front/public/vite.config.js
@@ -2,13 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    compression()
+    vue()
   ],
   resolve: {
     alias: {
