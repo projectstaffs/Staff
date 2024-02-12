065017296ef9091c2cc790d374afd7679c735830
Aleksandr-1987 <johndoe@example.com>
2024-02-12 16:46:35 +0200

    End

--- front/public/vite.config.js
+++ front/public/vite.config.js
@@ -2,13 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { compression } from 'vite-plugin-compression2'
+//import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    compression()
+    //compression()
   ],
   resolve: {
     alias: {
